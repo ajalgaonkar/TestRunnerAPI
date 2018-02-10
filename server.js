a02f9fb8c14502b4c43c78fac5eb0726c3ff012e
Aditya Jalgaonkar <adjalgao@microsoft.com>
2018-02-09 16:51:58 -0800

    Adding ScreenShots

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ var app = require('./app');
 var port = process.env.port || 3000;
 
 var server = app.listen(port, function(){
-    console.log('Express server listening on port ' + port);
+    console.log('TestRunner server listening on port ' + port);
 })
